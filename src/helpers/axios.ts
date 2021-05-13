48ce62ef43846bf56ef0ff3f5f86346bf24dcb87
Joao Paulo Furtado <joaopaulofurtado@live.com>
2021-05-12 23:10:12 -0700

    fixed leads

--- src/helpers/axios.ts
+++ src/helpers/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const customAxios = axios.create({
-  baseURL: "https://be.empregourgente.com",
+  baseURL: 'https://api.empregourgente.com',
   timeout: 10000,
   // headers: {'X-Custom-Header': 'foobar'}
 });
